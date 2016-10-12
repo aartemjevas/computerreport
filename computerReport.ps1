32c29a6f3dea20f58278a280bb4d9b76c5cbc9f5
Aleksandras Artemjevas <aleksandras.artemjevas@runwaybpo.com>
2016-10-12 16:54:05 +0300

    adding progress bar

--- computerReport.ps1
+++ computerReport.ps1
@@ -38,7 +38,7 @@ try
 if (Test-Path $ComputerList)
 {
     $Computers = Get-Content -Path $ComputerList
-    if (!($OutputPath))
+    if (!(Test-Path $OutputPath))
     {
         $null = mkdir $OutputPath
     }
@@ -49,9 +49,15 @@ if (Test-Path $ComputerList)
     else
     {
         $computersHTMLFragment = @()
+        $null = New-Item -Path $OutputPath -Name $LogName -ItemType File -Force -ErrorAction Stop
+        $logFile = Join-Path $OutputPath $LogName
+        $i = 0
+        $successCount = 0
+        $failedCount = 0
         foreach ($Computer in $Computers)
         {
             $Computer = $Computer.ToUpper()
+            Write-Progress -Activity "Gathering Data from $Computer"  -percentComplete ($i / $Computers.count*100)
             $obj = $null
             $obj  = New-Object Object
             $obj  | Add-Member Noteproperty 'Computername' -value $Computer
@@ -66,11 +72,18 @@ if (Test-Path $ComputerList)
                 $obj  | Add-Member Noteproperty 'Status' -value "Failed"
                 Write-Host "$Computer is offline" -ForegroundColor Yellow
             }
-            $computersHTMLFragment += Get-ComputerHTMLFragment -ComputersObject $obj -ErrorAction Stop   
+            $computersHTMLFragment += Get-ComputerHTMLFragment -ComputersObject $obj -ErrorAction Stop
+            Add-Content -Path $logFile -Value "$($obj.Timestamp) $($obj.Computername) $($obj.Status)" 
+            if ($obj.Status -eq "Succeeded") 
+            {
+                $successCount++
+            }
+            else
+            {
+                $failedCount++
+            }
+            $i++
         }
-        $successCount = ($ComputersObject | ? {$_.Status -eq "Succeeded"}).count
-        $failedCount = ($ComputersObject | ? {$_.Status -eq "Failed"}).count
-        #$computersHTMLFragment = Get-ComputersHTMLFragment -ComputersObject $ComputersObject -ErrorAction Stop
         New-HomeHTMLReport -ComputersHTMLFragment "$computersHTMLFragment" `
                            -LogName $LogName `
                            -Timestamp $(get-date -Format "yyyy-MM-dd HH:mm:ss") `
@@ -79,7 +92,6 @@ if (Test-Path $ComputerList)
                            -FailCount $failedCount `
                            -OutputPath $OutputPath `
                            -ErrorAction Stop
-
     }
 }
 else
